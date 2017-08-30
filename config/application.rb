548bdfca3b6d142c6ef56013cf022d0a4a483803
victor95pc <victorpalomocastro@gmail.com>
2017-08-30 09:06:00 -0300

    Translate application to portuguese

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module DesafioFulllab
 
     config.active_job.queue_adapter = :sidekiq
 
+    config.i18n.default_locale = "pt-BR"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
