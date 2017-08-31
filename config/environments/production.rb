842a6db80c09b6f8317d3af53552ea0431ed977c
victor95pc <victorpalomocastro@gmail.com>
2017-08-31 01:36:24 -0300

    Set default url host for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.default_url_options = { host: 'https://desafio-fulllab.herokuapp.com' }
+
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
