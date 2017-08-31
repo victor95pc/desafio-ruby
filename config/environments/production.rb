14d1e42c3d3d475f861302c643eeb5c4ef8d7be2
victor95pc <victorpalomocastro@gmail.com>
2017-08-30 23:03:31 -0300

    Configure e-mail sending on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,14 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
+
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'desafio-fulllab.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
